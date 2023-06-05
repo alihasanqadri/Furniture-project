a5abb967a445c1f789f8ce789b783ed0650c47fc
alihasanqadri <129279634+alihasanqadri@users.noreply.github.com>
2023-06-05 23:02:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let userAge=+prompt("Enter Your Age");
+
+ if(userAge < 18) {console.log("You are not eligible")
+ alert("You are not eligible");
+ }
+ else{console.log("You are eligible")
+ alert("You are eligible");
+ };
